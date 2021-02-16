0747062f63ad4fc3aecd664ee816dd8d1b4458f3
Philideli <74490209+Philideli@users.noreply.github.com>
2021-02-16 17:03:35 +0300

    Update main

--- App/Lab14/src/Main.java
+++ App/Lab14/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         try {
             Task10v2 ttt = new Task10v2();
             ttt.split("D:\\Games\\Maid.of.Sker.v1.0.42156-GOG\\123.bin","our",3000000000L);
+            Task4 task4 = new Task4();
+            task4.copeToUpperCase("source.txt", "destination.txt");
         }
         catch (Error err)
         {
