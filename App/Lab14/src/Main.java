b9dcf3731760fc8258cf33fc22532a67add37c8e
Hellamb <vlad.kuksiuk@gmail.com>
2021-02-16 17:05:46 +0200

    Task10 refactor

--- App/Lab14/src/Main.java
+++ App/Lab14/src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         try {
-            Task10v2 ttt = new Task10v2();
+            Task10 ttt = new Task10();
             ttt.split("D:\\Games\\Maid.of.Sker.v1.0.42156-GOG\\123.bin","our",3000000000L);
         }
         catch (Error err)
--- App/Lab14/src/Task10.java
+++ App/Lab14/src/Task10.java
@@ -5,44 +5,44 @@ public class Task10 {
     {
         try(BufferedInputStream fileInputStream =  new BufferedInputStream( new FileInputStream(source)))
         {
-            int value; //количество байт прочитанных из файла
-            long currentSize = maxSize;
-            int fileNum = 0;
-            while(currentSize > Integer.MAX_VALUE)
+            long currentSize = 0; //текущее количество прочитанных байтов, до смены файла
+            int bufferSize;   //размер буфера
+            if(maxSize>2048)   //
             {
-                currentSize -= Integer.MAX_VALUE;
-                byte[] buf = new byte[1024];
-                while((value = fileInputStream.read(buf) )!=-1)
-                {
-                    BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(destinationPrefix+"."+fileNum+".txt",true));
-                    bufferedOutputStream.write(buf,0,value);
-                }
-                if(currentSize <= Integer.MAX_VALUE)
-                {
-                    byte[] buf2 = new byte[(int)currentSize];
-                    while((value = fileInputStream.read(buf) )!=-1)
-                    {
-                        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(destinationPrefix+"."+fileNum+".txt", true));
-                        bufferedOutputStream.write(buf2,0,value);
-                        bufferedOutputStream.close();
-                        fileNum++;
-
-                    }
-                }
+                bufferSize = 2048;
+            }else
+            {
+                bufferSize = (int)maxSize;
             }
-            if(maxSize > Integer.MAX_VALUE) return;
-
-            byte[] buf = new byte[(int)currentSize];
-            while((value = fileInputStream.read(buf) )!=-1)
+            int lastPoint = bufferSize; //количество байт, которые нужно считать, до предела буфера либо файла
+            int value;  //количество байт прочитанных из файла
+            int fileNum = 0; //номер файла
+            boolean isLast = false; //флаг для проверки, нужно ли менять номер файла
+            byte[] buf = new byte[lastPoint];
+            while((value = fileInputStream.read(buf,0, lastPoint))!=-1)
             {
                 BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(destinationPrefix+"."+fileNum+".txt",true));
                 bufferedOutputStream.write(buf,0,value);
+                currentSize+= value;
+
+                if(isLast) //меняет номер файла и сбрасывает значения до изначальных
+                {
+                    fileNum++;
+                    lastPoint = bufferSize;
+                    isLast = false;
+                    currentSize = 0;
+                }
+
+                if((maxSize-currentSize) < bufferSize) // если выполняется, то считает не более чем нужно за следующий заход и изменит номер файла(на следующем заходе)
+                {
+                    lastPoint = (int)(maxSize-currentSize);
+                    isLast = true;
+
+                }
                 bufferedOutputStream.close();
-                fileNum++;
 
             }
         }
 
     }
 }
-
