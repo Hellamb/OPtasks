30af873a85d09d072e17868806a83e7398c0d11a
Hellamb <vlad.kuksiuk@gmail.com>
2021-03-10 18:36:35 +0200

    SomeChanges

--- Lab1/src/main/java/com/example/Lab1/DimaPageServlet.java
+++ Lab1/src/main/java/com/example/Lab1/DimaPageServlet.java
@@ -4,20 +4,18 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "DimaPage", value = "/DimaPageServlet")
+public class DimaPageServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><head></head><body>");
-        out.println("<h1>Hello from HelloServlet</h1>");
-        out.println("<img src = 'https://cs8.pikabu.ru/post_img/big/2016/02/04/7/145458292112119207.jpg'></br>");
-        out.println("<a href = 'https://www.google.com.ua'>To Google<a></br>");
+        out.println("<h1>Hello from DimaPageServlet</h1>");
+        out.println("<img src = ''></br>");
         out.println("<a href = 'KseniaPageServlet'>To Ksenia Page</a></br>");
         out.println("<a href = 'VladPageServlet'>To Vlad Page</a></br>");
-        out.println("<a href = 'DimaPageServlet'>To Dima Page</a></br>");
         out.println("</body></html>");
     }
 
--- Lab1/src/main/java/com/example/Lab1/VladPageServlet.java
+++ Lab1/src/main/java/com/example/Lab1/VladPageServlet.java
@@ -4,19 +4,17 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "VladPage", value = "/VladPageServlet")
+public class VladPageServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><head></head><body>");
-        out.println("<h1>Hello from HelloServlet</h1>");
-        out.println("<img src = 'https://cs8.pikabu.ru/post_img/big/2016/02/04/7/145458292112119207.jpg'></br>");
-        out.println("<a href = 'https://www.google.com.ua'>To Google<a></br>");
+        out.println("<h1>Hello from VladPageServlet</h1>");
+        out.println("<img src = 'Images/VladPhoto.jpg'></br>");
         out.println("<a href = 'KseniaPageServlet'>To Ksenia Page</a></br>");
-        out.println("<a href = 'VladPageServlet'>To Vlad Page</a></br>");
         out.println("<a href = 'DimaPageServlet'>To Dima Page</a></br>");
         out.println("</body></html>");
     }
