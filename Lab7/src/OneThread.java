80d8940d315450787363254eb756021420983395
Philideli <k.ivanchenko-2027@kpi.ua>
2021-04-09 10:27:08 +0300

    lab7_final

--- Lab7/src/OneThread.java
+++ Lab7/src/OneThread.java
@@ -13,18 +13,12 @@ public class OneThread implements Runnable, CountWords{
     public void run() {
         for(int i = 0;i< input.size();i++){
             try {
-                commonestWords(input.get(i), output);
+                CountWords.count(new BufferedReader(new FileReader(input.get(i))),
+                        new BufferedWriter(new FileWriter(output,true)));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-    }
-
-    private void commonestWords(String inFilename, String outFilename) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(inFilename));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(outFilename,true));
-
-        CountWords.count(reader,writer);
     }
 
 }
